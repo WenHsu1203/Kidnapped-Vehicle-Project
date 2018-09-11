902c030894109c371adf2f1b3b610a0ff0f05ac0
WenHsu1203 <wenhsu1203@g.ucla.edu>
2018-09-11 11:05:18 -0700

    Update README

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # Introduction
->My robot has been kidnapped and transported to a new location. Luckily it has `a map` of this location, a (noisy) `GPS estimate of its initial location`, and lots of `noisy sensor` and control data.
+My robot has been kidnapped and transported to a new location. Luckily it has `a map` of this location, a (noisy) `GPS estimate of its initial location`, and lots of `noisy sensor` and control data.
 To Save this robot, I implemented a `2 dimensional particle filter` in C++. This filter is given a map and some initial localization information (analogous to what a GPS would provide). At each time step your filter will also get observation and control data.
 
 __What I have: Initial Localization from GPS, Observation, Velocity and Yaw Rate Data with Noise__ 
-* 
+* Observation data are the measured distance of landmarks
+
 __What my GOALs are: Implementing the Particle Filter to Localize the Robot__
 1. Initialize `50 particles` based on x, y, theta from GPS with random Gaussian noise
 2. In each time step, predict states of particles with velocity and yaw_rate input, using `CTRV`(Constant Trun Rate and Velocity) model
 3. Associate data with observations through `nearest neighbor` method, then update the particles weights by calculating the product of each measurement's `Multivariate-Gaussian probability density`
 4. Resample the particles based on the new weights
+
+>**Here is the flow chart of particle filter**
 ![Particle Filter Flow Chart](https://github.com/WenHsu1203/Kidnapped-Vehicle-Project/blob/master/Photo/Particle%20Filter.png?raw=true)
 
+# Result
+In the simulator you can see the path that the robot drives along with all of its `landmark measurements(⨷) `represented by `green lines`. The estimation of the position from particle is the `blue circle` around the robot
 
+__*Demo Video☟*__
+[![Video](http://img.youtube.com/vi/0rbb8DnsY6g/0.jpg)](http://www.youtube.com/watch?v=0rbb8DnsY6g "Particle Filter")
 
 # Download
 Simulator can be downloaded [here](https://github.com/udacity/self-driving-car-sim/releases)
--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -23,10 +23,9 @@ using namespace std;
 static default_random_engine gen;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
+	// Set the number of particles. Initialize all particles to first position (based on estimates of 
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
-	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	normal_distribution<double> dist_x(x,std[0]);
 	normal_distribution<double> dist_y(y,std[1]);
 	normal_distribution<double> dist_theta(theta,std[2]);
@@ -50,10 +49,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
-	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
-	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
-	//  http://www.cplusplus.com/reference/random/default_random_engine/
+	// Add measurements to each particle and add random Gaussian noise.
 	normal_distribution<double> dist_x(0.0,std_pos[0]);
 	normal_distribution<double> dist_y(0.0,std_pos[1]);
 	normal_distribution<double> dist_theta(0.0,std_pos[2]);
@@ -76,10 +72,8 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
-	// TODO: Find the predicted measurement that is closest to each observed measurement and assign the 
+	// Find the predicted measurement that is closest to each observed measurement and assign the 
 	//   observed measurement to this particular landmark.
-	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
-	//   implement this method and use it as a helper during the updateWeights phase.
 	for (int i = 0 ; i < observations.size(); i++)
 	{
 		double min_dist = dist(predicted[0].x, predicted[0].y, observations[i].x,observations[i].y);
@@ -100,16 +94,7 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		const std::vector<LandmarkObs> &observations, const Map &map_landmarks) {
-	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
-	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
-	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
-	//   according to the MAP'S coordinate system. You will need to transform between the two systems.
-	//   Keep in mind that this transformation requires both rotation AND translation (but no scaling).
-	//   The following is a good resource for the theory:
-	//   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-	//   and the following is a good resource for the actual equation to implement (look at equation 
-	//   3.33
-	//   http://planning.cs.uiuc.edu/node99.html
+	// Update the weights of each particle using a mult-variate Gaussian distribution.
 	double sig_x = std_landmark[0];
 	double sig_y = std_landmark[1];
 
@@ -159,9 +144,7 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 }
 
 void ParticleFilter::resample() {
-	// TODO: Resample particles with replacement with probability proportional to their weight. 
-	// NOTE: You may find std::discrete_distribution helpful here.
-	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
+	// Resample particles with replacement with probability proportional to their weight. 
 	std::vector<Particle> particles_;
 	int index = rand() % num_particles;
 	double beta = 0.0;
